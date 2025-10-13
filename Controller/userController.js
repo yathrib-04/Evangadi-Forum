39684d0b7673ef656e5129b33b4f462a32e6178e
teyiba12 <teyibaaman68@gmail.com>
2025-10-13 10:17:16 +0300

    add user login route with JWT authentication and password validation

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,12 +1,67 @@
-function register(req, res) {
-    res.send("User registered");
-}
-function login(req, res) {
-    res.send("User logged in");
+// db connection 
+const dbconnection = require("../DB/dbConfig")
+const bcrypt = require("bcrypt");
+const {StatusCodes} = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+
+async function register(req, res) {
+    const {username, firstname, lastname, email, password} = req.body;
+    if (!username || !firstname || !lastname || !email || !password) {
+        return res.status(StatusCodes.BAD_REQUEST).json({ message: "All fields are required" });
+    }
+  
+    try {
+        const [user] = await dbconnection.query("select username, userid from users WHERE username = ? or email = ?", [username, email]);
+        if (user.length > 0) {
+            return res.status(StatusCodes.CONFLICT).json({ message: "User already exists" });
+        }
+        if (password.length <= 8) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ message: "Password must be at least 8 characters long" });
+        }
+        // Hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        await dbconnection.query("INSERT INTO users (username, firstname, lastname, email, password) VALUES (?, ?, ?, ?, ?)", [username, firstname, lastname, email, hashedPassword]);
+        return res.status(StatusCodes.CREATED).json({ message: "User registered successfully" });
+    } catch (error) {
+        console.log(error.message);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "something went wrong please try again" });
+
+    }
 
 }
 
-function check(req, res) {
+async function login(req, res) {
+    const {email, password} = req.body;
+    if (!email || !password) {
+        return res.status(StatusCodes.BAD_REQUEST).json({ message: "All fields are required" });
+    }
+    try {
+        const [user] = await dbconnection.query("select userid, username,email, password from users WHERE email = ?", [email]);
+        if (user.length == 0) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ message: "Invalid credentials" });
+        }
+
+        // Compare the password    and the following line return boolean value
+        const isMatch = await bcrypt.compare(password, user[0].password);
+        if (!isMatch) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ message: "Invalid credentials" });
+        }
+       
+        const username = user[0].username;
+        const userid = user[0].userid;
+        const token = jwt.sign({ userid, username }, "SecretKey", { expiresIn: "1h" });
+        return res.status(StatusCodes.OK).json({ message: "User logged in successfully", token });
+
+
+    } catch (error) {
+        console.log(error.message);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "something went wrong please try again" });
+    }
+}
+
+async function check(req, res) {
     res.send("User is logged in");
 }
 
--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const userRoutes = require("./routes/userRoutes");
+
 const dbconnection = require("./DB/dbConfig");
 
 // Middleware to parse JSON
 app.use(express.json());
 
+const userRoutes = require("./routes/userRoutes");
 // User routes
 app.use("/api/users", userRoutes);
 
