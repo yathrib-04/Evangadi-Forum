ca140cd7cd220b70a477a44d46779e387fbdfd85
teyiba12 <teyibaaman68@gmail.com>
2025-11-22 11:49:38 +0300

    Configure Axios instance with base URL for API requests

---
+++ frontend/src/axiosConfig.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const axiosBase = axios.create({
+  baseURL: 'http://localhost:5000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default axiosBase;
